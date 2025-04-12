58b74b2b2f718967d3571c176db3e9da7271c25c
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-04-12 13:42:20 +0545

    carousel created with javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,30 @@
-new Swiper('.trending-now_cards', {
-    // Optional parameters
-  
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-
-   
+const carousel = document.querySelector('.carousel');
+  const leftBtn = document.querySelector('.left-btn');
+  const rightBtn = document.querySelector('.right-btn');
+function scrollCarousel(directon){
+  const carousel = document.querySelector(".carousel");
+  const scrollAmount =220;
+  carousel.scrollBy({
+    left: directon*scrollAmount,
+    behavior: 'smooth'
   });
+  setTimeout(checkScrollButtons, 200);
+}
+  function checkScrollButtons(){
+    if (carousel.scrollLeft==0){
+      leftBtn.style.visibility='hidden';
+    }else{
+      leftBtn.style.visibility='visible';
+
+    }
+    if (carousel.scrollLeft + carousel.clientWidth >= carousel.scrollWidth - 1) {
+      rightBtn.style.visibility = 'hidden';
+    } else {
+      rightBtn.style.visibility = 'visible';
+    }
+
+  }
+  checkScrollButtons();
+
+  // Also run check when user scrolls manually
+  carousel.addEventListener('scroll', checkScrollButtons);
